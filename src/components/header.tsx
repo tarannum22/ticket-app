0a9f3413adc17135612ffb5b0c064e4c3d876cb8
Abul Tarannum <abul.tarannum@intersport.de>
2025-07-04 14:23:49 +0200

    add navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,23 @@
-export function Header(){
+import {HomeIcon, File, UserRound} from "lucide-react";
+import Link from 'next/link';
+import {NavButton} from "@/components/NavButton";
+
+export function Header() {
     return (
-        <p>
-            Header
-        </p>
+        <header className="animate-slide bg-background h-12 p-2 border-b sticky top-0 z-20">
+            <div className="flex h-8 items-center justify-between w-full">
+                <div className="flex items-center gap-2">
+                    <NavButton icon={HomeIcon} label="Home" href="/home"/>
+                    <Link href="/home" className="flex justify-center items-center gap-2 ml-0" title="Home">
+                        <h1 className="hidden sm:block text-xl font-bold m-0 mt-1">Computer Repair Shop</h1>
+                    </Link>
+                </div>
+                <div className="flex items-center">
+                    <NavButton icon={File} label="Tickets" href="/tickets"/>
+                    <NavButton icon={UserRound} label="Custoemrs" href="/customers"/>
+                </div>
+
+            </div>
+        </header>
     )
 }
